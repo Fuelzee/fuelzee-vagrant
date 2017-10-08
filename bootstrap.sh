f8967a302a1f4225fbb1cd367af3239bf934366c
Rodrigo Recio <rjrecio@gmail.com>
2017-10-08 20:54:06 -0300

    added requirement for rvm: gpg2 fixed "dpkg-reconfigure: unable to re-open stdin: No file or directory" problem adding DEBIAN_FRONTEND=noninteractive

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-
+export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y build-essential mongodb-server elasticsearch
+apt-get install -y gnupg2 build-essential mongodb-server elasticsearch
+sudo gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 source /etc/profile.d/rvm.sh
 rvm install ruby-2.0.0-p598
